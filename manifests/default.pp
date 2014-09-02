7af7d474b2eaab2943ce5476864aabfe879a0cf6
Joseph Gilley <joe.gilley@gmail.com>
2014-09-02 15:27:24 -0400

    Tests

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,8 +1,15 @@
+#node default {
+#	include git
+#	git::repo{'scales_puppet':
+#		path   => '/home/vagrant/puppet',
+#		source => 'git://github.com/joegilley/scales-puppet.git',
+#		branch => 'master',
+#	}
+#}
 node default {
-	include git
-	git::repo{'scales_puppet':
-		path   => '/home/vagrant/puppet',
-		source => 'git://github.com/leechfarm/scales-puppet.git',
-		branch => 'master',
+	vcsrepo { '/home/vagrant/puppet':
+		ensure   => present,
+		provider => git,
+		source   => 'git://github.com/joegilley/scales-puppet.git',
 	}
 }
